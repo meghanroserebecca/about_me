86263b16a5d4b0a0011ae5b106f078ac0b78e0f7
meghanroserebecca <meghanroserebecca@gmail.com>
2016-11-08 14:02:41 -0800

    Updated JS, CSS & HTML

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 console.log('Hello from app.js');
 
-var firstYesOrNoQuestionAnswer = prompt('I challenge you to a battle of wits. For the Princess! To the Death! Do you accept?')
+var firstYesOrNoQuestionAnswer = prompt('I challenge you to a battle of wits. For the Princess! To the Death! Do you accept?');
 
 if (firstYesOrNoQuestionAnswer.toLowerCase() === 'yes' ||
 firstYesOrNoQuestionAnswer.toLowerCase() == 'y') {
@@ -16,7 +16,7 @@ else {
   alert('Truly, you have a dizzying intellect.');
 }
 
-var secondYesOrNoQuestionAnswer = prompt('Do you smell anything?')
+var secondYesOrNoQuestionAnswer = prompt('Do you smell anything?');
 
 if (secondYesOrNoQuestionAnswer.toLowerCase() === 'no' ||
 secondYesOrNoQuestionAnswer.toLowerCase() === 'n') {
@@ -24,7 +24,7 @@ secondYesOrNoQuestionAnswer.toLowerCase() === 'n') {
 }
 else if (secondYesOrNoQuestionAnswer.toLowerCase() === 'yes' ||
 secondYesOrNoQuestionAnswer.toLowerCase() === 'y'){
-  alert('You are a liar. You smell nothing, and what you do not smell is iocane powder. It is odorless, tasteless, dissolves instantly in liquid and is among the most deadly poisons known to man.')
+  alert('You are a liar. You smell nothing, and what you do not smell is iocane powder. It is odorless, tasteless, dissolves instantly in liquid and is among the most deadly poisons known to man.');
 }
 else {
   alert('Truly, you have a dizzying intellect. This battle of wits is over before it\'s begun');
@@ -34,22 +34,22 @@ if (secondYesOrNoQuestionAnswer.toLowerCase() === 'yes' ||
 secondYesOrNoQuestionAnswer.toLowerCase() === 'y' ||
 secondYesOrNoQuestionAnswer.toLowerCase() === 'no' ||
 secondYesOrNoQuestionAnswer.toLowerCase() === 'n') {
-var thirdYesOrNoQuestionAnswer = prompt('Turn away. Good! Now you may look. I have placed the iocane powder in one of our two wine glasses. The battle of wits has begun. It ends when you decide and we both drink who is right and who is dead. All right, do you drink from your own cup?')
+  var thirdYesOrNoQuestionAnswer = prompt('Turn away. Good! Now you may look. I have placed the iocane powder in one of our two wine glasses. The battle of wits has begun. It ends when you decide and we both drink who is right and who is dead. All right, do you drink from your own cup?');
 
-if (thirdYesOrNoQuestionAnswer.toLowerCase() === 'yes'||
+  if (thirdYesOrNoQuestionAnswer.toLowerCase() === 'yes' ||
 thirdYesOrNoQuestionAnswer.toLowerCase() === 'y') {
-  alert('You have died. They were both poisoned. I spent the last few years building up an immunity to iocane powder.');
-}
-else if (thirdYesOrNoQuestionAnswer.toLowerCase() === 'no' ||
+    alert('You have died. They were both poisoned. I spent the last few years building up an immunity to iocane powder.');
+  }
+  else if (thirdYesOrNoQuestionAnswer.toLowerCase() === 'no' ||
 thirdYesOrNoQuestionAnswer.toLowerCase() === 'n') {
-  alert('Then drink deep of my cup, as I drink deep of yours. Now, you have died. They were both poisoned. I spent the last few years building up an immunity to iocane powder.');
-}
-else {
-  alert('Truly, you have a dizzying intellect.');
-}
+    alert('Then drink deep of my cup, as I drink deep of yours. Now, you have died. They were both poisoned. I spent the last few years building up an immunity to iocane powder.');
+  }
+  else {
+    alert('Truly, you have a dizzying intellect.');
+  }
 }
 
-var fourthYesOrNoQuestionAnswer = prompt('Have you ever heard of Plato? Aristotle? Socrates?')
+var fourthYesOrNoQuestionAnswer = prompt('Have you ever heard of Plato? Aristotle? Socrates?');
 
 if (fourthYesOrNoQuestionAnswer.toLowerCase() === 'yes' ||
 fourthYesOrNoQuestionAnswer.toLowerCase() === 'y') {
@@ -60,5 +60,19 @@ fourthYesOrNoQuestionAnswer.toLowerCase() === 'n') {
   alert('You are a moron.');
 }
 else {
-  alert('Are we even speaking the same language, here?')
+  alert('Are we even speaking the same language, here?');
+}
+
+var fifthYesOrNoQuestionAnswer = prompt('Do you want to get into a land war in Asia, or challenge a Sicilian when DEATH is on the line?');
+
+if (fifthYesOrNoQuestionAnswer.toLowerCase() === 'yes' ||
+fifthYesOrNoQuestionAnswer.toLowerCase() === 'y') {
+  alert('Clearly, you are a moron.');
+}
+else if (fifthYesOrNoQuestionAnswer.toLowerCase() === 'no' ||
+fifthYesOrNoQuestionAnswer.toLowerCase() == 'n') {
+  alert('Then at least you are not a COMPLETE moron!');
+}
+else {
+  alert('Truly, you have a dizzying intellect.')
 }
